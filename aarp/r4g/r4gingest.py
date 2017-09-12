c743818df64a27239f8abf61ee0b7abe07713ab8
Swapnil Prabhu <sprabhu@highground.com>
2017-09-12 15:22:35 +0530

    Removed dag related details.

--- aarp/r4g/r4gingest.py
+++ aarp/r4g/r4gingest.py
@@ -7,17 +7,14 @@ import requests
 import json, yaml
 
 
-#Read yaml file
-CONFIG = {}
-
-with open('r4gingest.yaml') as f:
-    CONFIG = yaml.load(f)
-f.close()
-
-print(CONFIG['localpath'])
-
-
 def filelanding():
+    #airflow_env=loadEnvVariables()
+    
+    
+    with open('r4gingest.yaml') as f:
+        CONFIG = yaml.load(f)
+    f.close()
+    
     s = sftp.Connection(host=CONFIG['host'],username=CONFIG['ingestuser'],password=CONFIG['ingestpwd'])
 
     localpath = CONFIG['localpath']
@@ -41,6 +38,9 @@ def filelanding():
 
 
 def checkclusterstatus(clusterid):
+    with open('r4gingest.yaml') as f:
+        CONFIG = yaml.load(f)
+    f.close()
     clusterstatus = ""
     postdata = {
         "cluster_id": clusterid
@@ -71,6 +71,11 @@ def checkclusterstatus(clusterid):
 
 def jobrun(paramjson):
     print(paramjson["clusterid"])
+    
+    with open('r4gingest.yaml') as f:
+        CONFIG = yaml.load(f)
+    f.close()
+    
     postdata = {
       "run_name": "airflow_test_job",
       "existing_cluster_id":paramjson["clusterid"],
@@ -98,162 +103,5 @@ def jobrun(paramjson):
     print(res.content)
 
 
-def delcluster(clustid):
-    print(clustid)
-
-    postdata = {
-            "cluster_id": clustid
-            }
-    url = "https://dbc-db50c5d5-5ae4.cloud.databricks.com/api/2.0/clusters/delete"
-
-    res = requests.post(url, auth=(CONFIG['user'],CONFIG['pwd']), json=postdata)
-    print(res.content)
 
 
-dag = DAG(
-dag_id='r4gupload',
-start_date=datetime(2017, 7, 14),
-catchup= False,
-schedule_interval='@daily')
-
-
-t1 = PythonOperator(
-    task_id='dwld_files_task',
-    python_callable=filelanding(),
-    dag=dag
-)
-
-
-t2 = PythonOperator(
-    task_id='startcluster_task',
-    python_callable=checkclusterstatus("0706-084812-ulnas345"),
-    dag=dag
-)
-
-t2.set_upstream(t1)
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" : CONFIG['localfile'],
-    "schemaname":CONFIG['localschema']
-    }
-
-t3 = PythonOperator(
-    task_id='jobrun_task3',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t3.set_upstream(t2)
-
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" :CONFIG['auctionfile'],
-    "schemaname":CONFIG['auctionschema']
-    }
-
-t4 = PythonOperator(
-    task_id='jobrun_task4',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t4.set_upstream(t2)
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" : CONFIG['merchfile'],
-    "schemaname":CONFIG['merchschema']
-    }
-
-t5 = PythonOperator(
-    task_id='jobrun_task5',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t5.set_upstream(t2)
-
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" : CONFIG['pointfile'],
-    "schemaname":CONFIG['pointschema']
-    }
-
-t6 = PythonOperator(
-    task_id='jobrun_task6',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t6.set_upstream(t2)
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" : CONFIG['promofile'],
-    "schemaname":CONFIG['promoschema']
-    }
-
-t7 = PythonOperator(
-    task_id='jobrun_task7',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t7.set_upstream(t2)
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" : CONFIG['regfile'],
-    "schemaname":CONFIG['regschema']
-    }
-
-t8 = PythonOperator(
-    task_id='jobrun_task8',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t8.set_upstream(t2)
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" : CONFIG['sweepile'],
-    "schemaname":CONFIG['sweepschema']
-    }
-
-t9 = PythonOperator(
-    task_id='jobrun_task9',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t9.set_upstream(t2)
-
-jobrunjson = {
-    "clusterid":t2.python_callable,
-    "filetype" : CONFIG['travelfile'],
-    "schemaname":CONFIG['travelschema']
-    }
-
-t10 = PythonOperator(
-    task_id='jobrun_task10',
-    python_callable=jobrun(jobrunjson),
-    dag = dag
-)
-t10.set_upstream(t2)
-
-
-t11 = PythonOperator(
-    task_id='delculster_task',
-    python_callable=delcluster(t2.python_callable),
-    dag = dag
-)
-
-
-t11.set_upstream(t3)
-t11.set_upstream(t4)
-t11.set_upstream(t5)
-t11.set_upstream(t6)
-t11.set_upstream(t7)
-t11.set_upstream(t8)
-t11.set_upstream(t9)
-t11.set_upstream(t10)
-
-
-#t11.set_upstream(t6)
