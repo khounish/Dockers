4ded0ae69027ec9c8d5e58821e5683007662c286
Swapnil Prabhu <sprabhu@highground.com>
2017-09-14 20:27:57 +0530

    code changes for the testing done on 9/14/2017

--- aarp/mnthlyscores/mnthlyscores.py
+++ aarp/mnthlyscores/mnthlyscores.py
@@ -4,22 +4,23 @@ from datetime import datetime , timedelta
 from airflow.operators.python_operator import PythonOperator
 import requests
 import json, yaml
-from aarp.common.utils import loadYAMLEnvVariables,checkForProdCluster,clusteridcreate
+from aarp.common.utils import loadYAMLEnvVariables,checkForProdCluster,createCluster
 
-CONFIG=loadEnvVariables()
+#CONFIG=loadEnvVariables()
+CONFIG=loadYAMLEnvVariables()
 clusterid=checkForProdCluster(CONFIG['cluster_name'])
 
 if clusterid['cluster_id'] is null:
-	clusterid=clusteridcreate(CONFIG['cluster_name'])
+	#clusterid=clusteridcreate(CONFIG['cluster_name'])
+	clusterid=createCluster(CONFIG['cluster_name'])
 
-
-def mnthlyscores:     
+def mnthlyscores():     
     postdata = {
       "run_name": "mnthlyscores",
       "existing_cluster_id":clusterid['cluster_id'],
       "notebook_task": {
         "notebook_path": CONFIG['imax']['mnthlyscores']['notebook_path'],
-        "base_parameters":{"pathDataLakeImax":CONFIG['imax']['mnthlyscores']['pathDataLakefcom'],"pathLandingImax":CONFIG['imax']['mnthlyscores']["pathLandingfcom"]}
+        "base_parameters":{"pathDataLakeImax":CONFIG['imax']['mnthlyscores']['pathDataLakescores'],"pathLandingImax":CONFIG['imax']['mnthlyscores']["pathDataLakescores"]}
       }
     }
     url = "https://dbc-db50c5d5-5ae4.cloud.databricks.com/api/2.0/jobs/runs/submit"
